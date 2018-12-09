b78d95cdbdbe356875fd0b15b9dc8684e5339031
thomasgatterer <thomas.gatterer@uibk.ac.at>
2018-12-09 22:50:44 +0100

    Update UpgrPSandDotNET.ps1

--- UpgrPSandDotNET.ps1
+++ UpgrPSandDotNET.ps1
@@ -8,3 +8,15 @@ Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
 
 # version can be 3.0, 4.0 or 5.1
 &$file -Version 5.1 -Username $username -Password $password -Verbose
+
+
+
+
+
+# this isn't needed but is a good security practice to complete
+Set-ExecutionPolicy -ExecutionPolicy Restricted -Force
+
+$reg_winlogon_path = "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon"
+Set-ItemProperty -Path $reg_winlogon_path -Name AutoAdminLogon -Value 0
+Remove-ItemProperty -Path $reg_winlogon_path -Name DefaultUserName -ErrorAction SilentlyContinue
+Remove-ItemProperty -Path $reg_winlogon_path -Name DefaultPassword -ErrorAction SilentlyContinue
